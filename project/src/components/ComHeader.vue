2d962bc5e1780b6ed0b9927a4fac223e8ed80a72
jina lee <jinalee@jinaui-MacBookPro.local>
2024-06-20 17:27:10 +0900

    헤더작업중

--- project/src/components/ComHeader.vue
+++ project/src/components/ComHeader.vue
@@ -1,11 +1,58 @@
 <template>
-    <div id="header">
-       header
-    </div>
+   <div id="header">
+      <!-- inner -->
+      <div class="inner">
+         <!-- option -->
+         <div class="option">
+            <div>
+               <button>화면크기</button>
+               <ul style="display:none">
+                  <li><button></button></li>
+                  <li><button></button></li>
+                  <li><button></button></li>
+                  <li><button></button></li>
+                  <li><button></button></li>
+                  <li><button></button></li>
+               </ul>
+            </div>
+            <div>
+               <button>Language</button>
+               <ul style="display:none">
+                  <li><a href=""></a></li>
+                  <li><a href=""></a></li>
+                  <li><a href=""></a></li>
+                  <li><a href=""></a></li>
+                  <li><a href=""></a></li>
+               </ul>
+            </div>
+         </div>
+         <!--// option -->
+         <div>
+            <div>
+               <h1><a href="">KSURE 사이버영업점</a></h1>
+               <ul>
+                  <li><a href="">기업</a></li>
+                  <li><a href="">은행</a></li>
+                  <li><a href="">특수</a></li>
+               </ul>
+            </div>
+            <ul>
+               <li><a href="">통합검색</a></li>
+               <li><a href="">로그인</a></li>
+               <li><a href="">회원가입</a></li>
+            </ul>
+         </div>
+      </div>
+      <!--// inner -->
+   </div>   
  </template>
  <script>
 
  </script>
+
+<style lang="scss">
+
+</style>
    
  
  
