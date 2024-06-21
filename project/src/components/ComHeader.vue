142f7ad3423c8f6411ddd8c7cde63a4bed610871
jina lee <jinalee@jinaui-MacBookPro.local>
2024-06-21 11:29:57 +0900

    헤더작업(피씨)

--- project/src/components/ComHeader.vue
+++ project/src/components/ComHeader.vue
@@ -10,9 +10,6 @@
                   <li><button></button></li>
                   <li><button></button></li>
                   <li><button></button></li>
-                  <li><button></button></li>
-                  <li><button></button></li>
-                  <li><button></button></li>
                </ul>
             </div>
             <div>
@@ -21,25 +18,23 @@
                   <li><a href=""></a></li>
                   <li><a href=""></a></li>
                   <li><a href=""></a></li>
-                  <li><a href=""></a></li>
-                  <li><a href=""></a></li>
                </ul>
             </div>
          </div>
          <!--// option -->
-         <div>
+         <div class="gnb">
             <div>
-               <h1><a href="">KSURE 사이버영업점</a></h1>
+               <h1><a href=""><span class="hide">KSURE 사이버영업점</span></a></h1>
                <ul>
-                  <li><a href="">기업</a></li>
+                  <li class="curr"><a href="">기업</a></li>
                   <li><a href="">은행</a></li>
                   <li><a href="">특수</a></li>
                </ul>
             </div>
             <ul>
-               <li><a href="">통합검색</a></li>
-               <li><a href="">로그인</a></li>
-               <li><a href="">회원가입</a></li>
+               <li><a href="" class="comSrchBtn">통합검색</a></li>
+               <li><a href="" class="loginBtn">로그인</a></li>
+               <li><a href="" class="signupBtn">회원가입</a></li>
             </ul>
          </div>
       </div>
@@ -51,7 +46,82 @@
  </script>
 
 <style lang="scss">
+   #header {
+      margin-top:32px; height:100px; border-bottom:1px solid #ccc;
 
+      .inner {
+         position:relative; margin:0 auto; width:1240px; max-width:100%;
+
+         .option {
+            display:flex; align-items:center; justify-content: flex-end; line-height:40px;
+
+            button {
+               display:inline-block; padding-right:18px; background:#fff url("@/assets/images/ico-select-s.png") no-repeat center right / 16px auto; border:none;
+            }
+
+            div + div > button {
+               position:relative; padding-left:24px;
+
+               &::before {
+                  position:absolute; content:""; top:1px; left:12px; width:1px; height:16px; background:#D8D8D8;
+               }
+            }
+         }
+
+         .gnb {
+            display:flex; align-items:center; justify-content:space-between; height:40px;
+            
+            > div {
+               display:flex; align-items:center;
+               
+               h1 {
+                  width:233px; height:36px; background:url("@/assets/images/logo.png") no-repeat center / 100% auto;
+               }
+
+               > ul {
+                  margin-left:48px;
+
+                  li {
+                     display:inline-block; 
+
+                     &:hover,
+                     &.curr {
+                        a {
+                           color:#EB003B;
+                        }
+                     }
+
+                     a {
+                        display:inline-block; padding:0 16px; font-weight:600; font-size:20px; color:#000;
+                     }
+                  }
+               }
+            }
+
+            > ul li {
+               display:inline-block;
+
+               & + li {
+                  margin-left:32px;
+               }
+
+               a {
+                  display:inline-block; padding-left:28px; font-weight:700; font-size:17px;
+
+                  &.comSrchBtn {
+                     background:url("@/assets/images/ico-srch.png") no-repeat center left/ 20px auto;
+                  }  
+                  &.loginBtn {
+                     background:url("@/assets/images/ico-login.png") no-repeat center left/ 20px auto;
+                  }
+                  &.signupBtn {
+                     background:url("@/assets/images/ico-signup.png") no-repeat center left/ 20px auto;
+                  }
+               }
+            }
+         }
+      }
+   }
 </style>
    
  
